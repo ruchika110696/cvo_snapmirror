47513f3b1c9129a15c8a8ac009f046e1519c81a3
Paniraj Koppa <paniraj.km@gmail.com>
2021-10-08 17:51:14 -0700

    Update resources.tf

--- resources.tf
+++ resources.tf
@@ -1,4 +1,4 @@
-
+/*
 # Resource to create CVO Cluster on AWS
 resource "netapp-cloudmanager_cvo_aws" "cvo-aws" {
   provider = netapp-cloudmanager
@@ -9,7 +9,16 @@ resource "netapp-cloudmanager_cvo_aws" "cvo-aws" {
   client_id             = var.connector_id
   writing_speed_state   = "NORMAL"
 }
+*/
+  
 
+variable "var1" {
+  default="pani"
+}
+
+output "o1" {
+  value=var.var1
+  
 /*
 #Resource to create a NFS Volume
 resource "netapp-cloudmanager_volume" "cvo-volume-nfs" {
