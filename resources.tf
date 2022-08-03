1432da465f0125ce97b85e109a5acf6f9beb2a7c
ruchika110696 <ruchikal@netapp.com>
2022-08-03 13:45:55 +0530

    updated resource.tf for gcp

--- resources.tf
+++ resources.tf
@@ -16,55 +16,29 @@ data "netapp-cloudmanager_cvo_aws" "on-prem-ontap" {
   client_id  = var.connector_id
 }
 
-
-#Resource to create a SINGLE NODE CVO Cluster on AWS
-resource "netapp-cloudmanager_cvo_aws" "cvo-aws" {
-  client_id           = var.connector_id
-  name                = var.name_of_cvo_cluster
-  svm_password        = var.cvo_admin_password
-  region              = var.region
-  subnet_id           = var.subnet
-  vpc_id              = var.vpc_id
-  writing_speed_state = "NORMAL"
-  license_type        = var.license_type
+#Resource to create a CVO Cluster on GCP
+resource "netapp-cloudmanager_cvo_gcp" "cvo-gcp" {
+  name = var.name_of_cvo_cluster
+  project_id = "flexpod-rtpopenlab"
+  zone = var.region
+  gcp_service_account = "abhinav3@netapp.com"
+  svm_password = var.cvo_admin_password
+  client_id = var.connector_id
+  workspace_id = "workspace-IDz6Nnwl"
+  gcp_label {
+        label_key = "name"
+        label_value = "cvo"
+      }
 }
-
-
-
-/*
-#Resource to create a Highly available CVO Cluster on AWS
-resource "netapp-cloudmanager_cvo_aws" "cvo-aws-ha" {
-  name                   = var.name_of_cvo_cluster
-  region                 = var.region
-  vpc_id                 = var.vpc_id
-  svm_password           = var.cvo_admin_password
-  client_id              = var.connector_id
-  is_ha                  = true
-  failover_mode          = var.failover_mode
-  node1_subnet_id        = var.node1_subnet_id
-  node2_subnet_id        = var.node2_subnet_id
-  mediator_subnet_id     = var.subnet_id_of_mediator
-  mediator_key_pair_name = var.keypair_name_for_mediator
-  cluster_floating_ip    = var.cluster_management_floating_IP
-  data_floating_ip       = var.data_floating_IP
-  data_floating_ip2      = var.data_floating_IP_2
-  svm_floating_ip        = var.svm_management_floating_IP
-  route_table_ids        = var.list_of_routing_table_ids
-  license_type           = var.license_type
-}
-*/
-
-
 
 #Resource to establish snapmirror relationship between on-prem and CVO
 resource "netapp-cloudmanager_snapmirror" "cl-snapmirror" {
   source_working_environment_id      = data.netapp-cloudmanager_cvo_aws.on-prem-ontap.id
-  destination_working_environment_id = netapp-cloudmanager_cvo_aws.cvo-aws.id
+  destination_working_environment_id = netapp-cloudmanager_cvo_gcp.cvo-gcp.id
   source_volume_name                 = var.source_volume
   source_svm_name                    = var.source_storage_vm_name
-  provider_volume_type               = "gp2"
   destination_volume_name            = var.destination_volume
-  destination_svm_name               = netapp-cloudmanager_cvo_aws.cvo-aws.svm_name
+  destination_svm_name               = netapp-cloudmanager_cvo_gcp.cvo-gcp.svm_name
   policy                             = "MirrorAllSnapshots"
   schedule                           = var.schedule_of_replication
   max_transfer_rate                  = "102400"
@@ -80,9 +54,9 @@ resource "netapp-cloudmanager_volume" "cvo-volume-nfs" {
   name                      = var.name_of_volume_to_create_on_cvo
   size                      = 1
   unit                      = "GB"
-  provider_volume_type      = "gp2"
+  provider_volume_type      = "pd-standard"
   export_policy_type        = "custom"
   export_policy_ip          = ["0.0.0.0/0"]
   export_policy_nfs_version = ["nfs4"]
-  working_environment_id    = netapp-cloudmanager_cvo_aws.cvo-aws.id
+  working_environment_id    = netapp-cloudmanager_cvo_gcp.cvo-gcp.id
 }
