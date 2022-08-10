b072f103b83a9f93f2009212fb3230286bcbc4dc
ruchika110696 <ruchikal@netapp.com>
2022-08-10 12:31:56 +0530

    Update resources.tf

--- resources.tf
+++ resources.tf
@@ -27,6 +27,7 @@ resource "netapp-cloudmanager_cvo_gcp" "cvogcp" {
   workspace_id = "workspaceK6sfbTRR"
   vpc_id = "flexpod-rtpopenlab-vpc-01"
   subnet_id = "flexpod-rtpopenlab-vpc-01-subnet-01"
+  license_type = "gcp-cot-premium-byol"
   gcp_label {
         label_key = "name"
         label_value = "cvo"
