8521587fcefa9e53deae83b47608ce615f5413f0
ruchika110696 <ruchikal@netapp.com>
2022-08-11 14:29:35 +0530

    Update resources.tf

--- resources.tf
+++ resources.tf
@@ -4,14 +4,14 @@ resource "netapp-cloudmanager_cvo_gcp" "cvogcp" {
   name = var.name_of_cvo_cluster
   project_id = "flexpod-rtpopenlab"
   zone = var.region
-  gcp_service_account = "connector@flexpod-rtpopenlab.iam.gserviceaccount.com"
+  #gcp_service_account = "connector@flexpod-rtpopenlab.iam.gserviceaccount.com"
   svm_password = var.cvo_admin_password
   client_id = var.connector_id
   workspace_id = "workspaceK6sfbTRR"
   vpc_id = "flexpod-rtpopenlab-vpc-01"
   subnet_id = "flexpod-rtpopenlab-vpc-01-subnet-01"
-  capacity_tier = "cloudStorage"
-  tier_level = "standard"
+  #capacity_tier = "cloudStorage"
+  #tier_level = "standard"
   capacity_package_name = "Freemium"
   gcp_label {
         label_key = "name"
