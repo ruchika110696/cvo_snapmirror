f36233f60b25eda597ab51b7b223d542b29a51f8
ruchika110696 <ruchikal@netapp.com>
2022-08-11 18:12:52 +0530

    Update resources.tf

--- resources.tf
+++ resources.tf
@@ -1,4 +1,19 @@
+#Resource to register FlexPod storage into CloudManager
+resource "netapp-cloudmanager_cvo_onprem" "cvo-onprem" {
+  name              = var.name_of_on-prem-ontap
+  cluster_address   = var.on-prem-ontap_cluster_ip
+  cluster_user_name = var.on-prem-ontap_user_name
+  cluster_password  = var.on-prem-ontap_user_password
+  client_id         = var.connector_id
+  location          = "ON_PREM"
+}
 
+#Data source to get fetach details of FlexPod details
+data "netapp-cloudmanager_cvo_aws" "on-prem-ontap" {
+  depends_on = [netapp-cloudmanager_cvo_onprem.cvo-onprem]
+  name       = var.name_of_on-prem-ontap
+  client_id  = var.connector_id
+}
 #Resource to create a CVO Cluster on GCP
 resource "netapp-cloudmanager_cvo_gcp" "cvogcp" {
   name = var.name_of_cvo_cluster
@@ -17,4 +32,35 @@ resource "netapp-cloudmanager_cvo_gcp" "cvogcp" {
         label_key = "name"
         label_value = "cvo"
       }
+}
+
+#Resource to establish snapmirror relationship between on-prem and CVO
+resource "netapp-cloudmanager_snapmirror" "cl-snapmirror" {
+  source_working_environment_id      = data.netapp-cloudmanager_cvo_aws.on-prem-ontap.id
+  destination_working_environment_id = netapp-cloudmanager_cvo_aws.cvo-aws.id
+  source_volume_name                 = var.source_volume
+  source_svm_name                    = var.source_storage_vm_name
+  provider_volume_type               = "gp2"
+  destination_volume_name            = var.destination_volume
+  destination_svm_name               = netapp-cloudmanager_cvo_aws.cvo-aws.svm_name
+  policy                             = "MirrorAllSnapshots"
+  schedule                           = var.schedule_of_replication
+  max_transfer_rate                  = "102400"
+  client_id                          = var.connector_id
+}
+
+
+
+#Resource to create a cloud volume on CVO
+resource "netapp-cloudmanager_volume" "cvo-volume-nfs" {
+  client_id                 = var.connector_id
+  volume_protocol           = "nfs"
+  name                      = var.name_of_volume_to_create_on_cvo
+  size                      = 1
+  unit                      = "GB"
+  provider_volume_type      = "gp2"
+  export_policy_type        = "custom"
+  export_policy_ip          = ["0.0.0.0/0"]
+  export_policy_nfs_version = ["nfs4"]
+  working_environment_id    = netapp-cloudmanager_cvo_aws.cvo-aws.id
 }
