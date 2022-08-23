b56626c21428006dc783035620bab877ed0fb4a7
ruchika110696 <ruchikal@netapp.com>
2022-08-23 20:07:03 +0530

    Update resources.tf

--- resources.tf
+++ resources.tf
@@ -38,8 +38,8 @@ resource "netapp-cloudmanager_cvo_gcp" "cvogcp" {
 resource "netapp-cloudmanager_snapmirror" "cl-snapmirror" {
   source_working_environment_id      = data.netapp-cloudmanager_cvo_aws.on-prem-ontap.id
   destination_working_environment_id = netapp-cloudmanager_cvo_gcp.cvogcp.id
-  source_volume_name                 = "Test_Vol1"
-  source_svm_name                    = "hybrid-cloud-svm"
+  source_volume_name                 = "test"
+  source_svm_name                    = "hybrid_cloud_2_svm"
   provider_volume_type               = "pd-balanced"
   destination_volume_name            = var.destination_volume
   destination_svm_name               = netapp-cloudmanager_cvo_gcp.cvogcp.svm_name
